107927f175b63029f8b8f8412e4fc33e2bb37859
Carlos Patrón <malzk66@gmail.com>
2021-06-19 18:49:33 -0600

    ListView

--- SML_test/boards/views.py
+++ SML_test/boards/views.py
@@ -1,19 +1,25 @@
 from django.db.models import Count
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, render
+from django.views.generic import UpdateView, ListView
+from django.utils import timezone
+from django.utils.decorators import method_decorator
 
 from .forms import NewTopicForm, PostForm
 from .models import Board, Post, Topic
 
 
-def home(request):
-    boards = Board.objects.all()
-    return render(request, 'home.html', {'boards': boards})
+class BoardListView(ListView):
+    model = Board
+    context_object_name = 'boards'
+    template_name = 'home.html'
+
 
 def board_topics(request, pk):
     board = get_object_or_404(Board, pk=pk)
     topics = board.topics.order_by('-last_updated').annotate(replies=Count('posts') - 1)
     return render(request, 'topics.html', {'board': board, 'topics': topics})
+
 
 @login_required
 def new_topic(request, pk):
@@ -35,11 +41,13 @@ def new_topic(request, pk):
         form = NewTopicForm()
     return render(request, 'new_topic.html', {'board': board, 'form': form})
 
+
 def topic_posts(request, pk, topic_pk):
     topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
     topic.views += 1
     topic.save()
     return render(request, 'topic_posts.html', {'topic': topic})
+
 
 @login_required
 def reply_topic(request, pk, topic_pk):
@@ -55,3 +63,23 @@ def reply_topic(request, pk, topic_pk):
     else:
         form = PostForm()
     return render(request, 'reply_topic.html', {'topic': topic, 'form': form})
+
+
+@method_decorator(login_required, name='dispatch')
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ('message', )
+    template_name = 'edit_post.html'
+    pk_url_kwarg = 'post_pk'
+    context_object_name = 'post'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(created_by=self.request.user)
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.updated_by = self.request.user
+        post.updated_at = timezone.now()
+        post.save()
+        return redirect('topic_posts', pk=post.topic.board.pk, topic_pk=post.topic.pk)
